53014d9abfe5191c78037eed1986564271a6ca78
THE KACHOO <225316206+THEKACHOO@users.noreply.github.com>
2026-06-11 03:21:48 +0700

    Update DOMHandler.js

--- CHOSONG/DOMHandler.js
+++ CHOSONG/DOMHandler.js
@@ -507,15 +507,12 @@ class DOMHandler {
 
     /**
      * Prepares song image DOM element
-     * FIXED: Langsung pakai URL, tidak melalui base64 (menghindari CORS)
      */
     setSongImage() {
-        // Langsung pakai URL cover seperti di screen 2 & 3
-        const coverUrl = this.songs[this.selectedSongIndex].albumCoverUrl;
-        const imgElement = document.querySelector(".song-image > .header > img");
-        if (imgElement && coverUrl) {
-            imgElement.setAttribute("src", coverUrl);
-        }
+        this.setBase64Image(
+            this.songs[this.selectedSongIndex].albumCoverUrl,
+            ".song-image > .header > img"
+        );
         this.setSongInfo();
         this.setSongLyrics(
             Array.from(document.querySelectorAll(".select-line.selected")).map(
